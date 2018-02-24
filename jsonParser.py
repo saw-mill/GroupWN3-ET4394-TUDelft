f1912a91ee08d5de84db3c9f5921307b5e793a7d
saw-mill <saumil.sachdeva@gmail.com>
2018-02-25 00:10:05 +0100

    Stores channel and Phy info in Dict. Prints graph using plotly

--- jsonParser.py
+++ jsonParser.py
@@ -1,18 +1,52 @@
 import json
+import collections
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.offline as offline
 
-with open('/home/sawmill/Documents/Wireshark/sample2.json') as f:
-	data=json.load(f)
-	#print(data)
+with open('Lecturehalldelta_21_02_channelhop_airodump.json') as f:
+    data=json.load(f)
 
-
+channelDistribution={}
+phyDistribution={}
+bssidChannelUse={}
 
 for packet in data:
-	wlanInfo=packet['_source']['layers']['wlan_radio']
-	if('wlan_radio.phy' in wlanInfo):
-		print wlanInfo['wlan_radio.phy']
+    wlanInfo=packet['_source']['layers']
 
-	if('wlan_radio.channel' in wlanInfo):
-		print wlanInfo['wlan_radio.channel']
+    channel =  int(str(wlanInfo['wlan_radio.channel'][0]))
+    if(channel not in channelDistribution):
+        channelDistribution.update({channel:1})     
+    else:
+        channelCount=channelDistribution.get(channel)
+        channelCount+=1
+        channelDistribution.update({channel:channelCount})
 
-	if('wlan_radio.frequency' in wlanInfo):
-		print wlanInfo['wlan_radio.frequency']
+    phy = int(str(wlanInfo["wlan_radio.phy"][0]))
+    if(phy not in phyDistribution):
+        phyDistribution.update({phy:1})     
+    else:
+        phyCount=phyDistribution.get(phy)
+        phyCount+=1
+        phyDistribution.update({phy:phyCount})
+
+    # bssid = int(str(wlanInfo["wlan.bssid"][0]))
+
+
+
+# orderedChannelDistribution=sorted(channelDistribution.items())
+orderedChannelDistribution=collections.OrderedDict(sorted(channelDistribution.items()))
+orderedPhyDistribution=collections.OrderedDict(sorted(phyDistribution.items()))
+
+labels = orderedChannelDistribution.keys()
+values = orderedChannelDistribution.values()
+trace = go.Pie(labels=labels, values=values, name='Channel Distribution')
+offline.plot([trace], image_filename='Channel Distribution', image='jpeg')
+
+# labels = orderedPhyDistribution.keys()
+# values = orderedPhyDistribution.values()
+# trace = go.Pie(labels=labels, values=values, name='Phy Distribution')
+# offline.plot([trace], image_filename='Phy Distribution', image='jpeg')
+
+    # frequency = 'Frequency : ' + wlanInfo['radiotap.channel.freq'][0] + '\n'
+    # print frequency 
