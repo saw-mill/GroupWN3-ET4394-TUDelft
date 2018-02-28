c999fbb748e6e4b0490b681f246bdab71dc28f85
niketagrawal <nickgrwl3@gmail.com>
2018-02-28 20:39:45 +0100

    Cited code and added description

--- FM_receiver_lab6.m
+++ FM_receiver_lab6.m
@@ -1,3 +1,10 @@
+%This code is taken from desktopSDR.com
+%This code implements a FM broadcast receiver which can be tested by using the RTL SDR device
+%The original code implements the multi stage process of downsampling and filtering (decimation) 
+%through a single function. We have broken down this component into two separate decimation units and
+%which individualy has the number by which the signal has to be downsampled and a low pass filter to 
+%avoid aliasing. We have used a window based FIR filter for this purpose.
+
 function FM_receiver_lab6
 
 %% PRINT FILE INFORMATION HYPERLINK TO COMMAND WINDOW
