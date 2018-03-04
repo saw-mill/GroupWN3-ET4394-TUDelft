0f6e8661f38480eb537b148e6205f45682788976
saw-mill <saumil.sachdeva@gmail.com>
2018-03-04 02:18:19 +0100

    Can Plot AP Phy Distribution

--- Wireshark/jsonParser.py
+++ Wireshark/jsonParser.py
@@ -6,7 +6,8 @@ import plotly.graph_objs as go
 import plotly.offline as offline
 from operator import itemgetter
 
-path='/home/sawmill/Documents/GroupWN3-WiresharkSniffingProject-TUDelft/Wireshark/Dataset/JSON'
+path='/home/sawmill/Documents/GroupWN3-ET4394-TUDelft/Wireshark/Dataset/JSON'
+os.chdir(path)
 for file in os.listdir(path):
     print file
     f=open(file)
@@ -15,33 +16,45 @@ for file in os.listdir(path):
     phyNumberToLetter={0:'Unknown',1:'FHSS',2:'IR',3:'DSSS',4:'B',5:'A',6:'G',7:'N',8:'AC'}
     channelDistribution={}
     phyDistribution={}
-    channelBssid={}
-
+    apBssid={}
+    apDistribution={}
     for packet in data:
         wlanInfo=packet['_source']['layers']
-
         # Recording Channel distribuition in a dict with channel as key and its count as value
-        channel =  int(str(wlanInfo['wlan_radio.channel'][0]))
+        try:
+            channel =  int(str(wlanInfo['wlan_radio.channel'][0]))
+            p = int(str(wlanInfo["wlan_radio.phy"][0]))
+            phy = phyNumberToLetter.get(p)
+            bssid = str(wlanInfo['wlan.bssid'][0])
+            if(['wlan_mgt.vht.op.channelwidth'][0] in wlanInfo):
+                apType='AC'
+            elif(['wlan_mgt.ht.capabilities'][0] in wlanInfo):
+                apType='N'
+            else:
+                apType=phy
+        except:
+            pass
         if(channel not in channelDistribution):
             channelDistribution.update({channel:1})     
         else:
             channelCount=channelDistribution.get(channel)
             channelCount+=1
             channelDistribution.update({channel:channelCount})
+    
+
+              
 
         # Recording bssid's that use a particular channel in a dict with channel as key and list of bssid's as value
-        bssid = str(wlanInfo['wlan.bssid'][0])
-        if (channel not in channelBssid):
-            channelBssid.update({channel:[bssid]})
-        else:
-            bssidList=channelBssid.get(channel)
-            if(bssid not in bssidList): #Storing only unique bssids per channel
-                bssidList.append(bssid)
-                channelBssid.update({channel:bssidList})
         
-        # Recording phy distribuition in a dict with phy as key and its count as value
-        p = int(str(wlanInfo["wlan_radio.phy"][0]))
-        phy = phyNumberToLetter.get(p) 
+        if (apType not in apBssid):
+            apBssid.update({apType:[bssid]})
+        else:
+            bssidList=apBssid.get(apType)
+            if(bssid not in bssidList): #Storing only unique bssids per Access Point
+                bssidList.append(bssid)
+                apBssid.update({apType:bssidList})
+        
+        # Recording phy distribuition in a dict with phy as key and its count as value 
         if(phy not in phyDistribution):
             phyDistribution.update({phy:1})     
         else:
@@ -50,11 +63,13 @@ for file in os.listdir(path):
             phyDistribution.update({phy:phyCount})
 
     # print(channelBssid)
-
+    for key,value in apBssid.iteritems():
+        apDistribution.update({key:len(value)})
+    # print apDistribution
     # orderedChannelDistribution=sorted(channelDistribution.items())
     orderedChannelDistribution=collections.OrderedDict(sorted(channelDistribution.items(), key=lambda t:t[1], reverse=True))
     orderedPhyDistribution=collections.OrderedDict(sorted(phyDistribution.items(), key=lambda t:t[1], reverse=True))
-
+    orderedAPDistribution=collections.OrderedDict(sorted(apDistribution.items(), key=lambda t:t[1], reverse=True))
     #Setting up Bar graph for Channel Distribution
     labels1 = orderedChannelDistribution.keys()
     values1 = orderedChannelDistribution.values()
@@ -64,9 +79,12 @@ for file in os.listdir(path):
         title='Channel Distribution '+'('+os.path.splitext(file)[0]+')',
         xaxis=dict(
             title='Channel Type',
+            dtick=1,
+            ticklen=8,
+            tickwidth=1,
         ),
         yaxis=dict(
-            title='Occurrence',
+            title='Packets',            
         )
     )
     fig1 = go.Figure(data=data, layout=layout)
@@ -77,16 +95,34 @@ for file in os.listdir(path):
     trace2 = go.Bar(x=labels2, y=values2, name='Phy Distribution')
     data=[trace2]
     layout = go.Layout(
-        title='Phy Distribution '+'('+os.path.splitext(file)[0]+')',
+        title='Message PHY Distribution '+'('+os.path.splitext(file)[0]+')',
         xaxis=dict(
             title='Phy Type',
         ),
         yaxis=dict(
-            title='Occurrence',
+            title='Packets',
         )
     )
     fig2 = go.Figure(data=data, layout=layout)
 
+    #Setting the Bar Graph for Types of Access points
+    labels3 = orderedAPDistribution.keys()
+    values3 = orderedAPDistribution.values()
+    trace3 = go.Bar(x=labels3, y=values3, name='Phy Distribution')
+    data=[trace3]
+    layout = go.Layout(
+        title='Access Point PHY Distribution '+'('+os.path.splitext(file)[0]+')',
+        xaxis=dict(
+            title='Phy Type',
+        ),
+        yaxis=dict(
+            title='Number of Access Points',
+        )
+    )
+    fig3 = go.Figure(data=data, layout=layout)
+
+
     #Creating the Graph
     offline.plot(fig1,filename=os.path.splitext(file)[0]+' Channel Distribution.html',image_filename=os.path.splitext(file)[0]+' Channel Distribution', image='jpeg')
     offline.plot(fig2,filename=os.path.splitext(file)[0]+' Phy Distribution.html',image_filename=os.path.splitext(file)[0]+' Phy Distribution', image='jpeg')
+    offline.plot(fig3,filename=os.path.splitext(file)[0]+' AP Distribution.html',image_filename=os.path.splitext(file)[0]+' AP Distribution', image='jpeg')
